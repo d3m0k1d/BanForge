fa156f7a6609130806904b75d7a3f02002420550
d3m0k1d <contact@d3m0k1d.ru>
2026-07-24 00:14:32 +0300

    fix: fix ci test job and logger error on test

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -15,27 +15,26 @@ type Logger struct {
 
 func New(debug bool) *Logger {
 	logDir := "/var/log/banforge"
-	if err := os.MkdirAll(logDir, 0750); err != nil {
-		return nil
-	}
-
-	fileWriter := &lumberjack.Logger{
-		Filename:   filepath.Join(logDir, "banforge.log"),
-		MaxSize:    500,
-		MaxBackups: 3,
-		MaxAge:     28,
-		Compress:   true,
-	}
-
 	var level slog.Level
 	if debug {
 		level = slog.LevelDebug
 	} else {
 		level = slog.LevelInfo
 	}
-	multiWriter := io.MultiWriter(fileWriter, os.Stdout)
 
-	handler := slog.NewTextHandler(multiWriter, &slog.HandlerOptions{
+	var output io.Writer = os.Stdout
+	if err := os.MkdirAll(logDir, 0750); err == nil {
+		fileWriter := &lumberjack.Logger{
+			Filename:   filepath.Join(logDir, "banforge.log"),
+			MaxSize:    500,
+			MaxBackups: 3,
+			MaxAge:     28,
+			Compress:   true,
+		}
+		output = io.MultiWriter(fileWriter, os.Stdout)
+	}
+
+	handler := slog.NewTextHandler(output, &slog.HandlerOptions{
 		Level: level,
 	})
 
